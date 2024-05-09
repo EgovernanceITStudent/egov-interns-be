1683c4d1b0806728b171a040aeb46dc0fdb69e9e
Kelechi Nwankwo <nkelechi21@gmail.com>
2024-05-09 10:30:01 +0100

    feature(database): connecting to postgresql using sequelize as the orm and expressjs and typescript [Delivers #187559992

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,15 @@
 import dotenv from 'dotenv'
 import { App } from './app'
+import  {Pool} from 'pg'
+import {Sequelize} from 'sequelize'
 dotenv.config()
 
+const connectionString = 'postgres://postgres.skjxbwzncomrweitcmfh:pHW9qZsUDZxf4xOU@aws-0-us-west-1.pooler.supabase.com:5432/postgres';
+//connecting to database
+const sequelize = new Sequelize(connectionString)
+
+module.exports = {
+    // querying:pool.query()
+}
 const app = new App([],3000);
 app.listen()
