625a7a59bcbfc9674db0c2683453f192b0565a9e
Samuel Adekoya <samueladexx@gmail.com>
2024-06-13 12:45:24 +0100

    implemented error handling middlewares [Finishes #187789541]

--- src/app.ts
+++ src/app.ts
@@ -1,43 +1,54 @@
-import express,{Application,Request,Response} from 'express';
-import { Controller } from './interfaces/controllerInterface';
-import cookieParser from 'cookie-parser';
-import cors from 'cors'
+import express, { Application, Request, Response } from "express";
+import { Controller } from "./interfaces/controllerInterface";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import pageNotFoundMiddleware from "./middlewares/404.middleware";
+import errorMiddleware from "./middlewares/error.middleware";
 
 export class App {
-    private express:Application;
-    controllers:Controller[];
-    port:number;
-    
+  private express: Application;
+  private controllers: Controller[];
+  private port: number;
 
-    constructor(controllers:Controller[],port:number){
-        this.express = express();
-        this.port = port;
-        this.controllers = controllers;
-        this.initiatializeMiddlewares();
-        this.initializeRoute()
-    }
-    
-    private initiatializeMiddlewares(){
-        this.express.use(cors())
-        this.express.use(express.json());
-        this.express.use(cookieParser());
-    }
+  constructor(controllers: Controller[], port: number) {
+    this.express = express();
+    this.port = port;
+    this.controllers = controllers;
 
-    private initializeRoute(){
-        this.express.get('/',(_req:Request,res:Response)=>{
-            res.json({
-                message:"welcome to the e-gov intern API"
-            })
-        });
+    this.initiatializeMiddlewares();
+    this.initializeRoutes();
+    this.initializeErrorHandling();
+  }
 
-        this.controllers.forEach(controller=>{
-            this.express.use(controller.path, controller.router)
-        })
-    }
+  private initiatializeMiddlewares() {
+    this.express.use(cors());
+    this.express.use(express.json());
+    this.express.use(cookieParser());
+  }
 
-    public listen(){
-        this.express.listen(this.port,()=>{
-            console.log('listening')
-        })
-    }
+  private initializeRoutes() {
+    this.express.get("/", (_req: Request, res: Response) => {
+      res.json({
+        message: "welcome to the e-gov intern API",
+      });
+    });
+
+    this.controllers.forEach((controller) => {
+      this.express.use(controller.path, controller.router);
+    });
+  }
+
+  private initializeErrorHandling(): void {
+    // catch 404 and forward to error handler
+    this.express.use(pageNotFoundMiddleware);
+
+    // error handler
+    this.express.use(errorMiddleware);
+  }
+
+  public listen() {
+    this.express.listen(this.port, () => {
+      console.log(`app listening on port ${this.port}`);
+    });
+  }
 }
